1ae969ac9b3a453d519510ccc5d787ef7bbf403c
apple <apple@192.168.0.105>
2019-08-22 17:43:09 +0530

    Added corn support and taking values from property files

--- TaskScheduler/src/main/java/com/example/taskscheduler/tasks/MyTask.java
+++ TaskScheduler/src/main/java/com/example/taskscheduler/tasks/MyTask.java
@@ -1,5 +1,8 @@
 package com.example.taskscheduler.tasks;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,9 +13,22 @@ public class MyTask {
 	@Value("${fixedDelay.in.milliseconds}")
 	private long fixedRate = 0;
 
-	@Scheduled(fixedRate = 1000)
-	public void myTaskSchedule() {
-		System.out.println("Task ran at: " + System.currentTimeMillis() / 1000);
+	@Scheduled(fixedRateString = "${fixedDelay.in.milliseconds}")
+	public void myFixedTaskSchedule() {
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+
+		System.out.println("Task ran using fixed rate at: " + dtf.format(now));
 		System.out.println("Delay frequency is: " + fixedRate);
+	}
+
+	@Scheduled(cron = "${cron.for.every.second}")
+	public void myTaskSchedule() {
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		
+		System.out.println("Task ran using cron config at: " + dtf.format(now));
 	}
 }
